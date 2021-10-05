e02caee42c8bf557ab64d9389321a5289e47fbba
Asbot5 <56194023+Asbot5@users.noreply.github.com>
2021-10-05 11:47:57 +0200

    Add files via upload

---
+++ OblFootballPlayer/FootballPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OblFootballPlayer
+{
+    public class FootballPlayer
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+        public FootballPlayer(int id, string name, int price, int shirtNumber)
+        {
+            _id = id;
+            _name = name;
+            _price = price;
+            _shirtNumber = shirtNumber;
+        }
+
+        public FootballPlayer()
+        {
+
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) throw new ArgumentOutOfRangeException();
+                _name = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException();
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value < 1 || value > 100) throw new ArgumentOutOfRangeException();
+                _shirtNumber = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Navn: {Name}, Pris: {Price}, Trøje nummer: {ShirtNumber}";
+        }
+    }
+}
---
+++ OblFootballPlayerTests/FootballPlayerTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OblFootballPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OblFootballPlayer.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer _footballPlayer;
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _footballPlayer = new FootballPlayer(1, "Bob", 1000, 5);
+        }
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => _footballPlayer.Name = "Bob");
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => _footballPlayer.Name = "");
+
+            _footballPlayer.Name = "Jens";
+            Assert.AreEqual("Jens", _footballPlayer.Name);
+
+            _footballPlayer.Name = "Asmus";
+            Assert.AreEqual("Asmus", _footballPlayer.Name);
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => _footballPlayer.Price = 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => _footballPlayer.Price = -1);
+
+            _footballPlayer.Price = 1;
+            Assert.AreEqual(1, _footballPlayer.Price);
+
+            _footballPlayer.Price = 1000;
+            Assert.AreEqual(1000, _footballPlayer.Price);
+        }
+
+        [TestMethod()]
+        public void ShirtNumberTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => _footballPlayer.ShirtNumber = 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => _footballPlayer.ShirtNumber = 101);
+
+            _footballPlayer.ShirtNumber = 1;
+            Assert.AreEqual(1, _footballPlayer.ShirtNumber);
+
+            _footballPlayer.ShirtNumber = 50;
+            Assert.AreEqual(50, _footballPlayer.ShirtNumber);
+
+            _footballPlayer.ShirtNumber = 100;
+            Assert.AreEqual(100, _footballPlayer.ShirtNumber);
+        }
+    }
+}
